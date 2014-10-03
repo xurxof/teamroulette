23ced92026d91652f6106c2e0ce853c86b0ff351
Miguel Gonzalez <migonzalvar@gmail.com>
2014-10-03 20:36:29 +0200

    Add template

--- teamroulette/urls.py
+++ teamroulette/urls.py
@@ -3,9 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'teamroulette.views.home', name='home'),
+    url(r'^$', 'teamroulette.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^test/$', 'teamroulette.views.test'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- teamroulette/views.py
+++ teamroulette/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def test(request):
-    return HttpResponse('Hola mundo')
+def home(request):
+    return render(request, 'index.html')
