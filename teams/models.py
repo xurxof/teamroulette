9809321c6bad9fdc6cae81649c5c3ca41ac9f744
Miguel Gonzalez <migonzalvar@gmail.com>
2014-10-03 23:36:05 +0200

    Add __str__ to models

--- teams/models.py
+++ teams/models.py
@@ -4,10 +4,18 @@ from django.db import models
 class Team(models.Model):
     name = models.CharField(max_length=128)
 
+    def __str__(self):
+        return self.name
+
 
 class Player(models.Model):
     name = models.CharField(max_length=128)
     team = models.ForeignKey(Team)
+
+    def __str__(self):
+        tpl = '{} ({})'
+        args = self.name, self.team
+        return tpl.format(*args)
 
 
 
