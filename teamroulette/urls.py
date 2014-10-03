eadce67ce46016d1d8c6ee9841d3b6816269a25f
Miguel Gonzalez <migonzalvar@gmail.com>
2014-10-03 18:41:19 +0200

    Initial import

---
+++ teamroulette/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'teamroulette.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^test/$', 'teamroulette.views.test'),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
