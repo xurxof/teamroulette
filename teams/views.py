6a5c37cfe76cf7cbaa1bfb99c3540d0927a39cbf
Miguel Gonzalez <migonzalvar@gmail.com>
2014-10-04 10:10:14 +0200

    Add form

--- teams/views.py
+++ teams/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from . import forms
 
 def main(request):
+    form = forms.TeamForm()
+    dictionary = {'name': 'Miguel',
+                  'form': form}
     return render(request,
                   'teams/index.html',
-                  dictionary={'name': 'Miguel'})
+                  dictionary=dictionary)
