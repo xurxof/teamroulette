dfc2562bfb67140b66f4ae5ac83741262437d3b8
Miguel Gonzalez <migonzalvar@gmail.com>
2014-10-04 12:40:21 +0200

    Enable authentication

--- teamroulette/settings.py
+++ teamroulette/settings.py
@@ -95,5 +95,9 @@ TEMPLATE_DIRS = (
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
 }
