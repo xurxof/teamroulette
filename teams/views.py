13f258d2fd103aad6a72eff6a3a7b2b6d3f1d56e
Miguel Gonzalez <migonzalvar@gmail.com>
2014-10-04 10:04:01 +0200

    Add context

--- teams/views.py
+++ teams/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def main(request):
-    return render(request, 'teams/index.html')
+    return render(request,
+                  'teams/index.html',
+                  dictionary={'name': 'Miguel'})
