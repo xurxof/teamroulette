ae524bb573e6fc9c5434fd4f6aeaacbc88ac5c10
Miguel Gonzalez <migonzalvar@gmail.com>
2014-10-04 09:53:21 +0200

    Add new view for teams

--- teamroulette/urls.py
+++ teamroulette/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'teamroulette.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^teams/', include('teams.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
