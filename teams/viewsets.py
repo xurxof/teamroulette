f39969e84eee39aaa7eab44b86db6cce02d38ffc
Miguel Gonzalez <migonzalvar@gmail.com>
2014-10-04 13:51:30 +0200

    Add owner permission

--- teams/viewsets.py
+++ teams/viewsets.py
@@ -1,8 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 from . import models
 
+from rest_framework.permissions import BasePermission
+
+class IsOwnerPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.owner
+
 class TeamViewSet(ModelViewSet):
     model = models.Team
+    permission_classes = (IsOwnerPermission, )
 
 class PlayerViewSet(ModelViewSet):
     model = models.Player
