6cd408de03802f85ccc0cd7fe4d3423e86be2dfc
Miguel Gonzalez <migonzalvar@gmail.com>
2014-10-04 10:24:58 +0200

    Handle POST

--- teams/views.py
+++ teams/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 from . import forms
 
 def main(request):
-    form = forms.TeamForm()
-    dictionary = {'name': 'Miguel',
-                  'form': form}
-    return render(request,
-                  'teams/index.html',
-                  dictionary=dictionary)
+    if request.method == 'GET':
+        form = forms.TeamForm()
+        dictionary = {'name': 'Miguel',
+                      'form': form}
+        return render(request,
+                      'teams/index.html',
+                      dictionary=dictionary)
+    elif request.method == 'POST':
+        form = forms.TeamForm(data=request.POST)
+        if form.is_valid():
+            assert False
+        else:
+            dictionary = {'name': 'Miguel',
+                          'form': form}
+            return render(request,
+                          'teams/index.html',
+                          dictionary=dictionary)
+
